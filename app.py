e2977e409b275ca4fb52fd53c8eef40936d87530
GAURANG SONKAVDE <76832579+Gaurang-Sonkavde@users.noreply.github.com>
2023-05-19 17:04:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 import streamlit as st
 import speech_recognition as sr
 import webbrowser
-import openai
+import openai 
 import os
 import datetime
 import random
-import numpy as np
-import win32com.client
+import numpy as np 
 import yfinance as yf
 
 chatStr = ""
@@ -24,9 +23,8 @@ def chat(query):
         frequency_penalty=0,
         presence_penalty=0
     )
-    speak(response["choices"][0]["text"])
-    chatStr += f"{response['choices'][0]['text']}\n"
     response_text = response["choices"][0]["text"]
+    chatStr += f"{response_text}\n"
     st.text_area('AI-Bot:', value=response_text)
     return response_text
 
@@ -51,11 +49,6 @@ def ai(prompt):
         f.write(text)
     
     return response["choices"][0]["text"]
-
-def speak(text):
-    speaker = win32com.client.Dispatch("SAPI.SPVoice")
-    Speak = speaker.Speak
-    Speak(text)
 
 def takeCommand():
     r = sr.Recognizer()
@@ -97,54 +90,57 @@ def main():
     query = user_input_col.text_input('User:')
     if send_col.button('Send'):
         if query:
+
             response = ""
             sites = [["youtube", "https://www.youtube.com"], ["wikipedia", "https://www.wikipedia.com"], ["google", "https://www.google.com"]]
             for site in sites:
                 if f"Open {site[0]}".lower() in query.lower():
                     response = "Opening requested website sir..."
-                    speak(f"Opening {site[0]} sir...")
+                    st.write(f"Opening {site[0]} sir...")
                     webbrowser.open(site[1])
 
-            #Open AI Answering
-            if "TM:".lower() in query.lower():
-                response = ai(prompt=query)
-                speak(response)
+        if "TM:".lower() in query.lower():
+            response = ai(prompt=query)
+            st.write(response)
 
-            elif "AI Bot Quit".lower() in query.lower():
-                exit()
+        elif "AI Bot Quit".lower() in query.lower():
+            exit()
 
-            elif "reset chat".lower() in query.lower():
-                chatStr = ""
+        elif "reset chat".lower() in query.lower():
+            chatStr = ""
 
-            elif "managed portfolio".lower() in query.lower():
+        elif "managed portfolio".lower() in query.lower():
 
-                stocks = ["RELIANCE.NS","TCS.NS","HDFCBANK.NS","HINDUNILVR.NS","INFY.NS","ICICIBANK.NS","KOTAKBANK.NS","AXISBANK.NS","LT.NS","SBIN.NS","ASIANPAINT.NS","HCLTECH.NS","WIPRO.NS","TECHM.NS","BHARTIARTL.NS","MARUTI.NS","NESTLEIND.NS","ONGC.NS","CIPLA.NS","DRREDDY.NS"]
-                # Replace with your desired stock symbols
-                returns_dict = {}
+            stocks = ["RELIANCE.NS", "TCS.NS", "HDFCBANK.NS", "HINDUNILVR.NS", "INFY.NS", "ICICIBANK.NS",
+                      "KOTAKBANK.NS", "AXISBANK.NS", "LT.NS", "SBIN.NS", "ASIANPAINT.NS", "HCLTECH.NS",
+                      "WIPRO.NS", "TECHM.NS", "BHARTIARTL.NS", "MARUTI.NS", "NESTLEIND.NS", "ONGC.NS",
+                      "CIPLA.NS", "DRREDDY.NS"]
+            # Replace with your desired stock symbols
+            returns_dict = {}
 
-                for stock in stocks:
-                    data = yf.Ticker(stock).history(period="1mo")  # Retrieve monthly data
-                    if len(data) > 0:
-                        monthly_returns = (data["Close"][-1] - data["Close"][0]) / data["Close"][0]
-                        returns_dict[stock] = monthly_returns
+            for stock in stocks:
+                data = yf.Ticker(stock).history(period="1mo")  # Retrieve monthly data
+                if len(data) > 0:
+                    monthly_returns = (data["Close"][-1] - data["Close"][0]) / data["Close"][0]
+                    returns_dict[stock] = monthly_returns
 
-                if returns_dict:
-                    sorted_stocks = sorted(returns_dict, key=returns_dict.get, reverse=True)
-                    top_stocks = sorted_stocks[:10]  # Select the top 10 stocks with highest returns
+            if returns_dict:
+                sorted_stocks = sorted(returns_dict, key=returns_dict.get, reverse=True)
+                top_stocks = sorted_stocks[:10]  # Select the top 10 stocks with highest returns
 
-                    response = "The highest profitable 10 stocks in your portfolio (based on monthly returns) are:\n"
-                    for stock in top_stocks:
-                        response += f"{stock}: {returns_dict[stock]:.2%}\n"
-                else:
-                    response = "No stock data available."
-
-                speak(response)
-
+                response = "The highest profitable 10 stocks in your portfolio (based on monthly returns) are:\n"
+                for stock in top_stocks:
+                    response += f"{stock}: {returns_dict[stock]:.2%}\n"
             else:
-                st.write("Chatting...")
-                # response = chat(query)
-            
-            st.text_area('AI-Bot:', value=response, height=400)
+                response = "No stock data available."
+
+            st.write(response)
+
+        else:
+            st.write("Chatting...")
+            # response = chat(query)
+
+        st.text_area('AI-Bot:', value=response, height=400)
 
     audio_query = speak_col.button("Speak")
     if audio_query:
@@ -153,5 +149,6 @@ def main():
         if query:
             response = chat(query)
             st.text_area('AI-Bot:', value=response, height=400)
+        
 if __name__ == '__main__':
     main()
